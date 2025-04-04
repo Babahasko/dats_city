98892a249a7ed3ed75d993fb2620dd44b27c8899
vasys.tapehin@gmail.com <vasys.tapehin@gmail.com>
2025-04-04 19:35:14 +0300

    добавил towers request

--- sender/game_parser.py
+++ sender/game_parser.py
@@ -2,7 +2,8 @@ from typing import Literal
 
 from pydantic import BaseModel
 
-
+# /api/words
+# Words list
 class GameState(BaseModel):
     mapSize: list[int]  # [x, y, z] размеры пространства башни
     nextTurnSec: int  # время до следующего хода в секундах
@@ -17,6 +18,7 @@ class WordPosition(BaseModel):
     id: int  # идентификатор слова
     pos: list[int]  # позиция слова в формате [x, y, z]
 
+#########################################################
 # Build tower or add words to existing tower. If tower is done, it will be saved and you can start new tower
 class BuildReq(BaseModel):
     done: bool  # флаг завершения строительства башни
@@ -25,3 +27,27 @@ class BuildReq(BaseModel):
 class BuildResponse(BaseModel):
     shuffleLeft: int  # количество оставшихся перемешиваний
     words: list[str]  # набор оставшихся слов
+##########################################################
+
+##########################################################
+# /api/towers GET
+# All towers
+class TowerWord(BaseModel):
+    dir: list[int]  # 0 - горизонтальное, 1 - вертикальное
+    pos: list[int]      # [x, y, z] координаты
+    text: str           # текст слова
+
+class TowerInfo(BaseModel):
+    score: float        # текущий счёт башни
+    words: list[TowerWord]  # слова в башне
+
+class DoneTower(BaseModel):
+    id: int             # ID завершённой башни
+    score: float        # счёт башни
+
+class GameResponse(BaseModel):
+    doneTowers: list[DoneTower]  # список завершённых башен
+    score: float                 # общий счёт
+    tower: TowerInfo             # текущая башня
+
+#############################################################
