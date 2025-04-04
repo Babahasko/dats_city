22ade92451cb0241362dcc4ac449947181f6d147
Babahasko <zakutni4ek@gmail.com>
2025-04-04 21:33:26 +0300

    push game rounds

--- scripts/game_rounds.py
+++ scripts/game_rounds.py
@@ -1,13 +1,16 @@
 import asyncio
 
 from sender import GameAPI
+from logic.logic import TowerBuilder
 game_api = GameAPI()
 
 async def main():
     game_data = await game_api.words()
     if game_data:
-        print(game_data)
         print(game_data.words)
+        tower_builder = TowerBuilder(words=game_data.words)
+        build_request = tower_builder.build_tower()
+
     if game_data is None:
         print()
 
