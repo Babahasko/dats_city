3c6b5f5f864d62bda0e54fc06e79b31cadaf6cb6
Babahasko <zakutni4ek@gmail.com>
2025-04-05 00:23:47 +0300

    check_logic

--- scripts/check_logic.py
+++ scripts/check_logic.py
@@ -1,39 +1,27 @@
-import random
-from typing import Tuple
-from logic.logic import TowerBuilder
-from utils.custom import convert_matrix_to_test_data
-from sender.send_on_front import send_data
+from logic.logic_opt2 import *
 
-def get_random_word(length: int):
-    import string
-    import random
-    result = ""
-    for _ in range(length):
-        result += random.choice(string.ascii_lowercase[:6])
-    return result
-
-# words = ["foundation", "support", "column", "beam", "floor",
-#      "wall", "ceiling", "structure", "building", "tower"]
-words = []
-for _ in range(1000):
-    words.append(get_random_word(random.randint(6,8)))
+if __name__ == "__main__":
 
 
-builder = TowerBuilder(words)
-tower = builder.build_tower()
-# builder.shuffle_words()
-tower = builder.continue_build()
-
-print(f"Башня построена! Счет: {tower['score']}")
-print(f"Высота: {tower['height']} этажей")
-print(f"Габариты: {tower['bounding_box']}")
-print("\nСтруктура башни:")
-for i, word in enumerate(tower['words']):
-    direction = "X" if word['dir'] == [1, 0, 0] else "Y" if word['dir'] == [0, 1, 0] else "Z"
-    print(f"{i+1}. {word['text']} ({direction}) at {word['pos']}")
-print(builder.build_requests)
-
-matrix = builder.construct_matrix_2()
-send_data(matrix)
+    # words = ["foundation", "support", "column", "beam", "floor",
+    #      "wall", "ceiling", "structure", "building", "tower"]
+    words = []
+    for _ in range(1000):
+        words.append(get_random_word(random.randint(6,10)))
 
 
+    builder = TowerBuilder(words)
+    tower = builder.build_optimized_tower()
+    builder.shuffle_words()
+    tower = builder.continue_build()
+
+    print(f"Башня построена! Счет: {tower['score']}")
+    print(f"Высота: {tower['height']} этажей")
+    # print(f"Габариты: {tower['bounding_box']}")
+    print("\nСтруктура башни:")
+    for i, word in enumerate(tower['words']):
+        direction = "X" if word['dir'] == (1, 0, 0) else "Y" if word['dir'] == (0, 1, 0) else "Z"
+        print(f"{i+1}. {word['text']} ({direction}) at {word['pos']}")
+    print(builder.build_requests)
+
+    print(builder.construct_matrix_2())
