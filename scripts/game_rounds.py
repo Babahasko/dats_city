0f2b4b010f21f6883d3d15e4ec347088e8127d9b
Babahasko <zakutni4ek@gmail.com>
2025-04-05 10:19:49 +0300

    use game_rounds

--- scripts/game_rounds.py
+++ scripts/game_rounds.py
@@ -3,6 +3,7 @@ import asyncio
 from sender import GameAPI
 from logic.logic import TowerBuilder
 from sender.game_parser import BuildReq
+from utils import logger
 
 game_api = GameAPI()
 
@@ -10,11 +11,12 @@ async def main():
     game_data = await game_api.words()
     if game_data:
         print(game_data.words)
-        tower_builder = TowerBuilder(words=game_data.words)
-        tower_builder.build_tower()
-        requests = tower_builder.build_requests
-        send_data = await game_api.build(BuildReq(done=False,words=requests))
-        print(send_data)
+        logger.info(game_data.words)
+        # tower_builder = TowerBuilder(words=game_data.words)
+        # tower_builder.build_tower()
+        # requests = tower_builder.build_requests
+        # send_data = await game_api.build(BuildReq(done=False,words=requests))
+        # print(send_data)
     if game_data is None:
         print()
 
